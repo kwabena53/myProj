562a46755451b21ca2316ff8d260bf580aa8085e
Kwabena Adu-Darkwa <Kwabenaadudarkwa@gmail.com>
2017-01-20 18:11:30 +0000

    multipy updated

--- Tester2.java
+++ Tester2.java
@@ -5,9 +5,6 @@ public class Tester2{
       if(args[0].equalsIgnoreCase("add")){
       ans=Integer.parseInt(args[1]) + Integer.parseInt(args[2]);
       }
-      else if (args[0].equalsIgnoreCase("sub")){
-      ans=Integer.parseInt(args[1]) - Integer.parseInt(args[2]);
-      }
       else if (args[0].equalsIgnoreCase("mult")){
       ans=Integer.parseInt(args[1]) * Integer.parseInt(args[2]);
       }
