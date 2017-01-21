3e66e7d6996c44f3c29e5afba607fc010597697f
Nii Boye <nii.quartey@ashesi.edu.gh>
2017-01-21 21:35:59 +0000

    'Last'

--- Calculator.java
+++ Calculator.java
@@ -12,16 +12,25 @@ public class Calculator {
       double c = Double.parseDouble(args[1]);
       double d = Double.parseDouble(args[2]);
       a = c + d; 
+      System.out.println(a);
     }
     else if (args[0].equalsIgnoreCase("Subtract")) {
       double x = Double.parseDouble(args[1]);
       double y = Double.parseDouble(args[2]);
       a = x - y;
+      System.out.println(a);
     } 
     else if (args[0].equalsIgnoreCase("Multiply")) {
       double m = Double.parseDouble(args[1]);
       double n = Double.parseDouble(args[2]);
       a = m * n;
+      System.out.println(a);
+    }
+    else if (args[0].equalsIgnoreCase("Divide")) {
+      double f = Double.parseDouble(args[1]);
+      double g = Double.parseDouble(args[2]);
+      a = f / g;
+      System.out.println(a);
     }
     
   }
